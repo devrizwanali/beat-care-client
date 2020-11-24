5e3158fcb92678dcf33a4a7acf5ca4a7dcd345e1
Rizwan Ali <rizwanali1055@gmail.com>
2020-11-24 23:03:14 +0500

    Update home page content.

--- src/components/TheHome.vue
+++ src/components/TheHome.vue
@@ -1,9 +1,8 @@
 <template>
-  <div class="mt-4" style="text-align: center;">
-    <b-card style="width: 50%; padding: 20px;margin: 30px;">
-      <b-card-text>
-       Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-      
+  <div class="mt-4" style="text-align: center; cursor: pointer;">
+    <b-card  calss="card" style="">
+      <b-card-text class="text-justify">
+      Heart rate or heartbeat is an important measure in health and fitness. BeatCare is developed to monitor one’s heart movements. The only thing needed is the monitoring of one’s pulse rates through an electrocardiogram device that is attached to the region around the heart. The application is able to record the users pulse rates and thereof the user is able to record notes. The user can record his heartbeat while at rest, while walking/exercising, and before receiving medication.
       </b-card-text>
       <b-button class="create-account" @click="createAccount">Create account</b-button>
     </b-card>
@@ -19,17 +18,26 @@
     }
   }
 </script>
-<style>
-  .create-account{
-    float: right;
-    color: #ccc;
-    font-weight: bold;
-    background: #3c4d69;
-  }
-
-  .create-account:hover {
-    background: #3c4d69;
-    color: white;
-  }
-
+<style scoped>
+.create-account{
+  float: right;
+  color: #ccc;
+  font-weight: bold;
+  background: #3c4d69;
+}
+.create-account:hover {
+  background: #3c4d69;
+  color: white;
+}
+.card {
+  transition: box-shadow .3s;
+  border-radius:10px;
+  width: 50%;
+  padding: 20px;
+  margin: 30px; 
+  border: 2px solid #3c4d69; 
+}
+.card:hover {
+  box-shadow: 0 0 11px rgba(33,33,33,.2); 
+}
 </style>
